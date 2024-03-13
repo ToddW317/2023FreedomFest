58466ae2561225061c8c6f3b00516026b8726a2b
Todd W <117058912+ToddW317@users.noreply.github.com>
2024-03-13 08:45:31 -0700

    events map

--- script.js
+++ script.js
@@ -177,4 +177,34 @@ const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropd
           }
         }
       });
+
+
+
+// Map Links
+
+function openMap(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Ensure the DOM is fully loaded before executing
+document.addEventListener("DOMContentLoaded", function() {
+    // Explicitly open the first tab
+    openMap(new Event('click'), 'Location1');
+});
       
