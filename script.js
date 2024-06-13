3bef3fca8f7eb8c1ccef441d028757c32f4a3fa6
Todd W. <117058912+ToddW317@users.noreply.github.com>
2024-06-13 02:32:25 -0700

    Updates to Logos, contact info, Homepage, Events. Added Gallery Page,

--- script.js
+++ script.js
@@ -98,50 +98,35 @@ const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropd
             new GalleryFilter()
 
 
-            // Countdown Timer
+// Countdown Timer
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
 
-            (function () {
-                const second = 1000,
-                      minute = second * 60,
-                      hour = minute * 60,
-                      day = hour * 24;
-              
-                //I'm adding this section so I don't have to keep updating this pen every year :-)
-                let today = new Date(),
-                    dd = String(today.getDate()).padStart(2, "0"),
-                    mm = String(today.getMonth() + 1).padStart(2, "0"),
-                    yyyy = today.getFullYear(),
-                    nextYear = yyyy + 1,
-                    dayMonth = "07/04/",
-                    birthday = dayMonth + yyyy;
-                
-                today = mm + "/" + dd + "/" + yyyy;
-                if (today > birthday) {
-                  birthday = dayMonth + nextYear;
-                }
-                //end
-                
-                const countDown = new Date(birthday).getTime(),
-                    x = setInterval(function() {    
-              
-                      const now = new Date().getTime(),
-                            distance = countDown - now;
-              
-                      document.getElementById("days").innerText = Math.floor(distance / (day)),
-                        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-              
-                      //do something later when date is reached
-                      if (distance < 0) {
-                        document.getElementById("headline").innerText = "Let the fun BEGIN!";
-                        document.getElementById("countdown").style.display = "none";
-                        document.getElementById("content").style.display = "block";
-                        clearInterval(x);
-                      }
-                      //seconds
-                    }, 0)
-                }());
+    // Set the target date for the event (July 4, 2024)
+    const eventDate = new Date("July 4, 2024 00:00:00").getTime();
+
+    const x = setInterval(function () {
+      const now = new Date().getTime(),
+            distance = eventDate - now;
+
+      document.getElementById("days").innerText = Math.floor(distance / day);
+      document.getElementById("hours").innerText = Math.floor((distance % day) / hour);
+      document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute);
+      document.getElementById("seconds").innerText = Math.floor((distance % minute) / second);
+
+      // Do something when the date is reached
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "Let the fun BEGIN!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+    }, 1000);
+  }());
+
 
     setInterval(function(){
         $('#list').stop().animate({scrollTop:40},400,'swing',function(){
